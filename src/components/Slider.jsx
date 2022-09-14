d6ac8224f6d21836d2fea3520cec8a1bbf824eb7
Igor Perdigão <igor_contatos@hotmail.com>
2022-09-13 23:19:36 -0300

    slider
    Atualizações no slider do projeto

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./Slider.css";
 import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
 import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
 
 export default function Slider(){
-    
+    const [click, setClick] = useState(1)
+    const firstPage = {transform: 'translateX(0)'}
+    const secondPage = {transform: 'translateX(-100vw)'}
+    const thirdPage = {transform: 'translateX(-200vw)'}
+ 
+    function returnPage(e){
+        if(e === 1){
+            return firstPage
+        } else if(e === 2){
+            return secondPage
+        } else if(e === 3){
+            return thirdPage
+        }
+    }
+
+    function changePage(){
+        setClick(click +1)
+        if( click > 2){
+            setClick(1)
+        }
+    } 
+
     return(
         <div className="slider-container">
-            <div className="arrow-left">
-                <ArrowLeftOutlinedIcon />
-            </div>
-
-            <div className="slider-wrapper">
+            <div className="slider-wrapper" style={returnPage(click)}>
                 <div className="slide">
                     <div className="img-container">
                         <img className="image" src="https://i.pinimg.com/originals/c9/a6/ee/c9a6ee03ac5fe905ea3b86bb97c54c45.png"></img>
@@ -47,8 +64,11 @@ export default function Slider(){
                 </div>
             </div>
 
-            <div className="arrow-right">
-                <ArrowRightOutlinedIcon />
+            <div className="arrow-left" onClick={() => changePage()}>
+                <ArrowLeftOutlinedIcon style={{fontSize: "40px"}}/>
+            </div>
+            <div className="arrow-right" onClick={() => changePage()}>
+                <ArrowRightOutlinedIcon style={{fontSize: "40px"}}/>
             </div>
         </div>
     )
