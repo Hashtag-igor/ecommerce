892f9f55a310243b1e0d36e1146ac39730411e86
Igor Perdigão <igor_contatos@hotmail.com>
2022-09-14 23:33:43 -0300

    slider 2.0
    Mais uma alteração do slider com informações passadas do data.js

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,12 +3,14 @@ import React, {useState} from "react";
 import "./Slider.css";
 import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
 import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
+import { sliderItems } from "../data";
 
 export default function Slider(){
     const [click, setClick] = useState(1)
-    const firstPage = {transform: 'translateX(0)'}
-    const secondPage = {transform: 'translateX(-100vw)'}
-    const thirdPage = {transform: 'translateX(-200vw)'}
+
+    const firstPage = {transform: 'translateX(0)', backgroundColor: "f5fafd"}
+    const secondPage = {transform: 'translateX(-100vw)', backgroundColor: "fcf1ed"}
+    const thirdPage = {transform: 'translateX(-200vw)', backgroundColor: "fbf0f4"}
  
     function returnPage(e){
         if(e === 1){
@@ -30,38 +32,18 @@ export default function Slider(){
     return(
         <div className="slider-container">
             <div className="slider-wrapper" style={returnPage(click)}>
-                <div className="slide">
+                {sliderItems.map((item)=>(
+                <div className="slide" bg={item.bg}>
                     <div className="img-container">
-                        <img className="image" src="https://i.pinimg.com/originals/c9/a6/ee/c9a6ee03ac5fe905ea3b86bb97c54c45.png"></img>
+                        <img className="image" src={item.img}></img>
                     </div>
                     <div className="info-container">
-                        <h1 className="title">SUMMER SALE</h1>
-                        <p className="desc">DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</p>
+                        <h1 className="title">{item.title}</h1>
+                        <p className="desc">{item.desc}</p>
                         <button className="button">SHOW NOW</button>
                     </div>
                 </div>
-
-                <div className="slide" style={{backgroundColor: "#fcf1ed"}}>
-                    <div className="img-container">
-                        <img className="image" src="https://i.pinimg.com/originals/c9/a6/ee/c9a6ee03ac5fe905ea3b86bb97c54c45.png"></img>
-                    </div>
-                    <div className="info-container">
-                        <h1 className="title">WINTER SALE</h1>
-                        <p className="desc">DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</p>
-                        <button className="button">SHOW NOW</button>
-                    </div>
-                </div>
-
-                <div className="slide"  style={{backgroundColor: "#fbf0f4"}}>
-                    <div className="img-container">
-                        <img className="image" src="https://i.pinimg.com/originals/c9/a6/ee/c9a6ee03ac5fe905ea3b86bb97c54c45.png"></img>
-                    </div>
-                    <div className="info-container">
-                        <h1 className="title">POPULAR SALE</h1>
-                        <p className="desc">DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</p>
-                        <button className="button">SHOW NOW</button>
-                    </div>
-                </div>
+                ))}
             </div>
 
             <div className="arrow-left" onClick={() => changePage()}>
@@ -73,3 +55,4 @@ export default function Slider(){
         </div>
     )
 }
+
